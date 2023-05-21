b2af47913bf2012f56ce4c482a4231cd9d516526
Emiliano <r.diazemiliano.red@gmail.com>
2023-05-20 21:47:41 -0300

    Parcial

---
+++ EvaluacionPV/Controllers/FormsController.cs
@@ -0,0 +1,38 @@
+﻿using EvaluacionPV.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace EvaluacionPV.Controllers
+{
+    public class FormsController : Controller
+    {
+
+
+       public static  List<IDtributaria> tributaria = new List<IDtributaria>();
+
+        // Resto del código del controlador
+
+        public IActionResult Index()
+        {
+            return View(tributaria);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IDtributaria tarea)
+        {
+            tarea.Id = tributaria.Count + 1;
+            tarea.Id = tributaria.Count + 1;
+            tributaria.Add(tarea);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+    
+
+
---
+++ EvaluacionPV/Models/IDtributaria.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvaluacionPV.Models
+{
+    public class IDtributaria
+    {
+        
+        
+            public int Id { get; set; }
+            public string? Cond_IVA { get; set; }
+            public string? CUIT { get; set; }
+            public string? Razón_social { get; set; }
+            public string? Domicilio { get; set; }
+            public string? Email { get; set; }
+            public string? CondicionesVenta { get; set; }
+
+        
+
+    }
+    }
+
+
